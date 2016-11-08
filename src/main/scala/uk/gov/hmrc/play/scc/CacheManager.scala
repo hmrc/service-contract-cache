7ae443dec4009e8f5e71627df9783f0a98dba0ec
abhishek.jain <abhishek.jain@digital.hmrc.gov.uk>
2016-11-08 13:35:30 +0000

    Play 2.5 upgrade

--- src/main/scala/uk/gov/hmrc/play/scc/CacheManager.scala
+++ src/main/scala/uk/gov/hmrc/play/scc/CacheManager.scala
@@ -30,49 +30,51 @@ import scala.language.implicitConversions
 class CacheManager(restCacheEndPoint: String, cache: CacheApi, ws: WSClient, timeToLive: Int) {
 
   def get[T](resource: String, attribute: Option[String] = None)(implicit read: Reads[T]): Future[T] = {
+
     val cacheKey = restCacheEndPoint + "/" + resource + "/" + attribute.getOrElse("")
-    cache.get[T](cacheKey) match {
-      case Some(data) =>
-        Future.successful(data.asInstanceOf[T])
-      case None =>
-        ws.url(cacheKey)
-          .get()
-          .flatMap {
-            case response if response.status == INTERNAL_SERVER_ERROR =>
-              Future.failed(new EndPoint500Exception)
-            case response if response.status == NOT_FOUND =>
-              Future.failed(new EndPoint404Exception)
-            case response if response.status == NO_CONTENT =>
-              Future.failed(new EndPoint204Exception)
-            case response if response.status == OK =>
-              if (attribute.isEmpty) {
-                cache.set(cacheKey, response.json.asInstanceOf[T], timeToLive)
-                Future {
-                  response.json.validate[T].get
-                }
-              } else {
-                val value = response.json \ attribute.get match {
-                  case JsDefined(res: JsValue) => {
-                    res match {
-                      case jsObj: JsObject => Future.successful(jsObj.asInstanceOf[T])
-                      case JsNumber(n) if n.isValidInt => Future.successful(n.bigDecimal.intValue())
-                      case JsString(s) => Future.successful(s)
-                      case JsBoolean(b) => Future.successful(b)
-                      case res => Future.failed(new UnSupportedDataType)
-                    }
-                  }
-                  case _ => {
-                    Future.failed(new UnSupportedDataType)
-                  }
-                }
-                value.map(value => {
-                  cache.set(cacheKey, value.asInstanceOf[T], timeToLive)
-                  value.asInstanceOf[T]
-                })
+
+    val data = cache.get[T](cacheKey)
+    if (data == null) {
+      ws.url(cacheKey)
+        .get()
+        .flatMap {
+          case response if response.status == INTERNAL_SERVER_ERROR =>
+            Future.failed(new EndPoint500Exception)
+          case response if response.status == NOT_FOUND =>
+            Future.failed(new EndPoint404Exception)
+          case response if response.status == NO_CONTENT =>
+            Future.failed(new EndPoint204Exception)
+          case response if response.status == OK =>
+            if (attribute.isEmpty) {
+              cache.set(cacheKey, response.json.asInstanceOf[T], timeToLive)
+              Future {
+                response.json.validate[T].get
               }
-            case response =>
-              Future.failed(new EndPointAllOtherExceptions(response.body))
-          }
+            } else {
+              val value = response.json \ attribute.get match {
+                case JsDefined(res: JsValue) => {
+                  res match {
+                    case jsObj: JsObject => Future.successful(jsObj.asInstanceOf[T])
+                    case JsNumber(n) if n.isValidInt => Future.successful(n.bigDecimal.intValue())
+                    case JsString(s) => Future.successful(s)
+                    case JsBoolean(b) => Future.successful(b)
+                    case res => Future.failed(new UnSupportedDataType)
+                  }
+                }
+                case _ => {
+                  Future.failed(new UnSupportedDataType)
+                }
+              }
+              value.map(value => {
+                cache.set(cacheKey, value.asInstanceOf[T], timeToLive)
+                value.asInstanceOf[T]
+              })
+            }
+          case response =>
+            Future.failed(new EndPointAllOtherExceptions(response.body))
+        }
+    } else {
+      Future.successful(data.asInstanceOf[T])
     }
   }
 }
