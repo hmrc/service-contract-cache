ada1119eb5ad603a153b7538c442efd42a8b94ab
Chetan <75028018+CPardeep@users.noreply.github.com>
2024-02-06 15:09:49 +0000

    GLSD-9795: Play 3 Upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,20 +1,15 @@
 import sbt._
-import play.core.PlayVersion
 import play.sbt.PlayImport._
 
 private object AppDependencies {
 
-  lazy val compile: Seq[ModuleID] = Seq(
-    "com.typesafe.play" %% "play" % PlayVersion.current,
+  val compile: Seq[ModuleID] = Seq(
     ehcache,
     ws
   )
 
-  lazy val test = Seq(
-    "com.typesafe.play"       %% "play-test"                % PlayVersion.current % "test",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"             % "test",
-    "org.mockito"             %% "mockito-scala-scalatest"  % "1.16.3"            % "test",
-    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.35.10"           % "test"
+  val test = Seq(
+    "uk.gov.hmrc"    %% "bootstrap-test-play-30"    % "8.1.0"   % Test
   )
 
   def apply(): Seq[ModuleID] = compile ++ test
--- src/test/scala/uk/gov/hmrc/play/scc/CacheManagerFixtures.scala
+++ src/test/scala/uk/gov/hmrc/play/scc/CacheManagerFixtures.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 HM Revenue & Customs
+ * Copyright 2024 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,8 +16,8 @@
 
 package uk.gov.hmrc.play.scc
 
-import akka.stream.scaladsl.Source
-import akka.util.ByteString
+import org.apache.pekko.stream.scaladsl.Source
+import org.apache.pekko.util.ByteString
 import org.mockito.ArgumentMatchers
 import org.mockito.scalatest.MockitoSugar
 import play.api.cache.SyncCacheApi
